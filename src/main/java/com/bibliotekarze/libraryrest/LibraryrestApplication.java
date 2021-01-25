1b3b7cedb47ad1477e2cebf5c14d3a804a91d748
Damian <bajno.damian96@gmail.com>
2021-01-25 12:13:38 +0100

    Initial commit

---
+++ src/main/java/com/bibliotekarze/libraryrest/LibraryrestApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotekarze.libraryrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryrestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bibliotekarze/libraryrest/LibraryrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotekarze.libraryrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
