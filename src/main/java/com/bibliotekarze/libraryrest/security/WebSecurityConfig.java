7a72aab10d345baf96a54bcd6895a68ef2fa0bd4
Damian <37028915+noneXved@users.noreply.github.com>
2021-03-24 18:32:54 +0100

    Remove dead code

--- src/main/java/com/bibliotekarze/libraryrest/security/WebSecurityConfig.java
+++ src/main/java/com/bibliotekarze/libraryrest/security/WebSecurityConfig.java
@@ -13,37 +13,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.cors().and().csrf().disable();
     }
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        return new UserDetailsServiceImpl();
-//    }
-
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public DaoAuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-//        authProvider.setUserDetailsService(userDetailsService());
-//       // authProvider.setPasswordEncoder(passwordEncoder());
-//
-//        return authProvider;
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.authenticationProvider(authenticationProvider());
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//            .anyRequest().authenticated()
-//            .and()
-//            .formLogin().permitAll()
-//            .and()
-//            .logout().permitAll();
-//    }
 }
